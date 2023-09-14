847a7be533713a0edb4ea8e7456de924b90a8c68
Felipe Gangrel <felipefrog@gmail.com>
2023-09-13 21:41:23 -0300

    Adds example for concat

---
+++ src/arrays/07-concat.ts
@@ -0,0 +1,13 @@
+export const items = ["chocolate", "vanilla", "strawberry"];
+
+/**
+ * Concat
+ *
+ * Concatenates two or more arrays
+ */
+
+const items2 = ["rocky road", "neapolitan"];
+
+let allItems = items.concat(items2);
+
+console.log(allItems); // ["chocolate", "vanilla", "strawberry", "rocky road", "neapolitan"]
