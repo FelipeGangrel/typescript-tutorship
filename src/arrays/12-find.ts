7d1f018ac2fe32d0cb46f01f8dca52bdc5d5e2aa
Felipe Gangrel <felipefrog@gmail.com>
2023-09-15 20:22:53 -0300

    Adds examples for find and findIndex

---
+++ src/arrays/12-find.ts
@@ -0,0 +1,19 @@
+export const persons = [
+  { name: "John", age: 21 },
+  { name: "Peter", age: 31 },
+  { name: "Mary", age: 42 },
+];
+
+/**
+ * Find
+ *
+ * Returns the first element in the array that satisfies the provided testing function.
+ */
+
+const firstWithAgeGreaterThan30 = persons.find((person) => person.age > 30);
+
+console.log(firstWithAgeGreaterThan30); // { name: "Peter", age: 31 }
+
+const firstWithAgeGreaterThan50 = persons.find((person) => person.age > 50);
+
+console.log(firstWithAgeGreaterThan50); // undefined
---
+++ src/arrays/13-findIndex.ts
@@ -0,0 +1,19 @@
+export const items = ["apple", "banana", "orange"];
+
+/**
+ * Find Index
+ *
+ * Returns the index of the first element in the array that satisfies the provided testing function.
+ */
+
+const appleIndex = items.findIndex((item) => item === "apple");
+
+console.log(appleIndex); // 0
+
+const bananaIndex = items.findIndex((item) => item === "banana");
+
+console.log(bananaIndex); // 1
+
+const strawberryIndex = items.findIndex((item) => item === "strawberry");
+
+console.log(strawberryIndex); // -1
