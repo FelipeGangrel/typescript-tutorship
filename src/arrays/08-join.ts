364b9070218b531db45baa7e90973cf607e5bb66
Felipe Gangrel <felipefrog@gmail.com>
2023-09-15 19:50:00 -0300

    Adds example for join

---
+++ src/arrays/08-join.ts
@@ -0,0 +1,11 @@
+export const items = ["chocolate", "vanilla", "strawberry"];
+
+/**
+ * Join
+ *
+ * Joins all elements of an array into a string
+ */
+
+const allItems = items.join(", ");
+
+console.log(allItems); // chocolate, vanilla, strawberry
