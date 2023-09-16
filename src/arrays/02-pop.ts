17bc5efebb50e08d79701faed658630387c00226
Felipe Gangrel <felipefrog@gmail.com>
2023-09-16 14:37:18 -0300

    Fixes typo 😭

--- src/arrays/02-pop.ts
+++ src/arrays/02-pop.ts
@@ -6,9 +6,9 @@ const items = ["chocolate", "vanilla", "strawberry"];
  * Removes the last element from an array and returns that element. This method changes the length of the array.
  */
 
-const removedIte = items.pop();
+const removedItem = items.pop();
 
-console.log(removedIte); // "strawberry"
+console.log(removedItem); // "strawberry"
 console.log(items); // ["chocolate", "vanilla"]
 
 export {};
