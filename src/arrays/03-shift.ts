81887d0fab9b0117a8d7075949402b8b12b59e3e
Felipe Gangrel <felipefrog@gmail.com>
2023-09-16 14:39:31 -0300

    updates shift example

--- src/arrays/03-shift.ts
+++ src/arrays/03-shift.ts
@@ -6,9 +6,9 @@ const items = ["chocolate", "vanilla", "strawberry"];
  * Removes the first element from an array and returns that element. This method changes the length of the array.
  */
 
-const firstItem = items.shift();
+const removedItem = items.shift();
 
-console.log(firstItem); // "chocolate"
+console.log(removedItem); // "chocolate"
 console.log(items); // ["vanilla", "strawberry"]
 
 export {};
