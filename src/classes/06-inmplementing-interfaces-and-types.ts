57f97255009be649348b10d605ae18ecf22c31cb
Felipe Gangrel <felipefrog@gmail.com>
2023-10-15 11:26:55 -0300

    Adds example for classes

---
+++ src/classes/06-inmplementing-interfaces-and-types.ts
@@ -0,0 +1,55 @@
+/**
+ * Implementing interfaces (and types)
+ */
+
+/**
+ * You could use a type alias instead of an interface
+ *
+ * I, personally, prefer to use interfaces for implementing contracts like
+ * the one below and types for everything else
+ */
+
+interface Person {
+  name: string;
+  age: number;
+}
+
+class Student implements Person {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+const student = new Student("John Doe", 25);
+
+console.log(student); // Student { name: 'John Doe', age: 25 }
+
+/**
+ * We can also implement multiple interfaces (and types)
+ */
+
+type Employee = {
+  id: number;
+};
+
+class Teacher implements Person, Employee {
+  name: string;
+  age: number;
+  id: number;
+
+  constructor(name: string, age: number, id: number) {
+    this.name = name;
+    this.age = age;
+    this.id = id;
+  }
+}
+
+const teacher = new Teacher("Jane Doe", 30, 123);
+
+console.log(teacher); // Teacher { name: 'Jane Doe', age: 30, id: 123 }
+
+export {};
